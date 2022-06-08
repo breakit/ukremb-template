0e61b372a1cd72286b1c71706e6dd1d91bd0b13b
breakit <breakthejails@gmail.com>
2022-06-08 17:38:30 +0200

    Improvements scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,55 @@ jQuery(document).ready(function($) {
   });
 
 });
+
+jQuery(document).ready(function($) {
+    $('.amount-chooser input.checkbutton').on('change', function() {
+        $('.amount-chooser input.checkbutton').not(this).prop('checked', false);
+    });
+
+    $(".amount-chooser .textBox").focus(function() {
+        $(".amount-chooser .checkbutton").prop("checked", false);
+    });
+
+    $(".amount-chooser .checkbutton").change(function() {
+        if($(this).is(":checked")) {
+            $(".amount-chooser .textBox").val("");
+        }
+    });
+  });
+});
+
+// Render the PayPal button into #paypal-button-container
+paypal.Buttons({
+    // Set up the transaction
+    createOrder: function(data, actions) {
+        return actions.order.create({
+            purchase_units: [{
+                amount: {
+                    value: function() {
+                      return jQuery(".amount-chooser.checkbutton:checked").val();
+                    }
+                }
+            }],
+            shipping_type: 'PICKUP',
+            application_context: {
+              shipping_preference: 'NO_SHIPPING'
+            }
+        });
+    },
+    // Finalize the transaction
+    onApprove: function(data, actions) {
+        return actions.order.capture().then(function(orderData) {
+            // Successful capture! For demo purposes:
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+            var transaction = orderData.purchase_units[0].payments.captures[0];
+            alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+            // Replace the above to show a success message within this page, e.g.
+            // const element = document.getElementById('paypal-button-container');
+            // element.innerHTML = '';
+            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+            // Or go to another URL:  actions.redirect('thank_you.html');
+        });
+    }
+}).render('#paypal-button-container');
